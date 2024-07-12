49b33b84e15bd8c9e55e13a960526fcf9a5af4c2
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-07-12 10:26:27 +0530

    fixed color issue in djfyworks section

--- src/Components/Layout/DjfyWorks.jsx
+++ src/Components/Layout/DjfyWorks.jsx
@@ -5,7 +5,7 @@ const DjfyWorks = () => {
   return (
     <div className="bg-[#07091F] text-white font-montserrat w-full flex justibe flex-col pb-12 pt-0">
       <div className="heading_container w-full flex-3 flex-col my-9 md:my-14 pb-8 md:pb-12 gap-3">
-        <h1 className="heading text-[20px] md:text-[32px] leading-[39.01px]">
+        <h1 className="heading text-[22px] md:text-[32px] leading-[39.01px]">
           HERE&apos;S HOW <span className="text-[#EC3533]">DJFY WORKS</span>
         </h1>
         <span className="w-[50px] bg-[#EC3533] h-[4px]"></span>
--- src/Components/UI/WorksBox.jsx
+++ src/Components/UI/WorksBox.jsx
@@ -1,6 +1,6 @@
 const Box = ({ icon, title, description }) => {
   return (
-    <div className="relative md:w-[370px] w-[330px] font-montserrat md:h-[314px] h-[300px] rounded-[30px] border-[1px] bg-gradient-to-b from-[#1B3782] to-[#09173C] p-4 md:p-6 flex-3">
+    <div className="relative md:w-[370px] w-[330px] font-montserrat md:h-[314px] h-[300px] rounded-[30px] border-[1px] bg-gradient-to-b from-[#162461] to-[#070d27] p-4 md:p-6 flex-3">
       <div className="absolute -top-[38.5px] left-14 transform -translate-x-1/2 flex items-center justify-center w-[77px] h-[77px] rounded-full bg-gradient-to-r from-[#ED3E31] to-[#EDA231] flex-3">
         <i className={`${icon} text-white text-[2.4rem]`}></i>
       </div>
